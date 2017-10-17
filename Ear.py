0abea15da7fc9757dfceb31b12c81e8711dde376
Muhammad Yousef <Muhammad.wd@outlook.com>
2017-10-17 19:45:03 +0200

    I've splitted the text into a list of sentences

--- Ear.py
+++ Ear.py
@@ -1,8 +1,12 @@
 # Wattary's Ear
-# NOTE: this file requires PyAudio & Python Speech Recognition Engine because it uses the Microphone class
+# NOTE: this file requires NLTK & PyAudio & Python Speech Recognition Engine because it uses the Microphone class
 
-#Importing the module
+#Importing the modules
 import speech_recognition as sr
+import nltk
+from nltk.tokenize import sent_tokenize
+
+#-------------------------- Speech Recognition : Speech-to-text -----------------------------------#
 
 # obtaining audio from the microphone
 Ear = sr.Recognizer()
@@ -11,12 +15,24 @@ with sr.Microphone() as source:
     audio = Ear.listen(source)
 
 # recognize speech using Google Speech Recognition
+# for testing purposes, we're just using the default API key
+# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")` instead of `r.recognize_google(audio)`
+
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")` instead of `r.recognize_google(audio)`
+    # Saving recognized text into text variable
     text = Ear.recognize_google(audio)
-    print(text)
 except sr.UnknownValueError:
     print("I can't hear you, Could you repeat that please? ")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+#----------------------------------------- NLP Pipeline ------------------------------------------#
+
+# For testing purposes only
+text = """Hello, My name is Ahmad. I am 22 years old. I study computer science at MIT """
+
+# Performing sentence tokenization and saving the result into sentences list in order to parse each one seperately.
+sentences = sent_tokenize(text)
+
+
+
