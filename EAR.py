6f9ca21f817514e690f910d03e3bc4969df6d404
Muhammad Yousef <Muhammad.wd@outlook.com>
2017-10-21 01:39:43 +0200

    Removing repeated characters from user input using regex and wordnet corpus

--- EAR.py
+++ EAR.py
@@ -5,12 +5,12 @@
 import speech_recognition as sr
 import nltk
 from nltk.tokenize import regexp_tokenize
+from nltk.corpus import wordnet
 import re
-
 
 class EAR:
 
-    text = "I should've done that thing I didn't do"
+    text = "I loooooove yooouuu"
     tokens = []
 
     # Speech-To-Text
@@ -52,6 +52,22 @@ class EAR:
 
         for (pattern, repl) in patterns:
             self.text = re.sub(pattern, repl, self.text)
+
+    # Repeating eliminator
+    def replace(self, word):
+        repeat_regexp = re.compile(r'(\w*)(\w)\2(\w*)')
+        repl = r'\1\2\3'
+
+        if wordnet.synsets(word):
+            return word
+        repl_word = repeat_regexp.sub(repl, word)
+        if repl_word != word:
+            return self.replace(repl_word)
+        else:
+            return repl_word
+
+    def Eliminator(self):
+        self.tokens = [self.replace(token) for token in self.tokens]
 
     # Performing word tokenization over the resulted text and save the result into a new list of tokens called tokens
     def tokenize(self):
