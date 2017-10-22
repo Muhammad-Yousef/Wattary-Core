50d35496e05a2a6ecec48b85f17e7539a4c15536
Muhannad Raouf <mohanadraouf95@gmail.com>
2017-10-22 23:02:16 +0200

    Writting the recommender system in OOP paradigm and some comments

--- RECOMMENDER.py
+++ RECOMMENDER.py
@@ -8,47 +8,101 @@ import pandas as pd
 import sklearn
 from sklearn.neighbors import NearestNeighbors
 
-#-----------------------------------------------------------------------------------------------------#
+#----------------------------------------------------- OOP Version -----------------------------------#
+class RECOMMENDER:
+    def __init__(self, dataSet):
+        self.dataSet = pd.read_csv('DataSets/mtcars.csv')
+
+    def Initialize(self):
+        # Define the Columns's names of the Data set
+        self.dataSet.coulmns = ['carNames', 'Mpg', 'Cyl', 'disp', 'horsePower', 'drat', 'Wight', 'qsec', 'vs', 'am', 'Gear', 'carb']
+
+    def Display(self):
+
+        """Mosab's implementation"""
 
 
+    def SetTestValues(self, testValues = []):
+        # This Method will take a Dict instead of list in the Future
+        self.listOfValues = testValues
 
-# using pandas library to read our Dataset file.csv
+        return listOfValues
+
+
+    def Model(self, valueList = []):
+        """
+            in this example  i choose  random values ,
+            in the future this indexes will depend on the user's selection
+            in this demo the user want us to recommend a car with :
+            1- Number of galons per mile
+            2- the Horse Power
+            3- Number of gears of the car
+        """
+        '''
+        :return array of 2 arrays 
+        1st: the length between the beginning of the csv file  to the chosen value
+        2nd: the chosen value that well be recommended to the user
+        '''
+        # checking data in all rows for the columns 1,4 and 10
+        self.Data = self.dataSet.ix[:, (1, 4, 10)].values
+
+        # Fitting the Nearest Neighbors function to the data in our data set
+        self.Neighbors = NearestNeighbors(n_neighbors = 1).fit(self.Data)
+
+        # Getting the nearest value to the desired values in the data set
+        self.Output = self.Neighbors.kneighbors([valueList])
+
+        return self.Output
+
+
+    def outPutHandling(self):
+
+        """Mosab's implementation"""
+
+
+#--------------------------------------------------Main To Test---------------------------------------#
+
+    """Mosab's implementation"""
+
+#------------------------------------- Old Version of the code without OOP paradigm-------------------#
+'''
+# using pandas library to read our Data set file.csv
 Cars = pd.read_csv('DataSets/mtcars.csv')
 
 def Initlaize():
 
-    # Define the Columns's names of the Dataset
+    # Define the Columns's names of the Data set
     Cars.columns = ['carNames', 'Mpg', 'Cyl', 'disp', 'horsePower', 'drat', 'Wight', 'qsec', 'vs', 'am', 'Gear', 'carb']
 
 
 def Display():
-    """ This method for Testing purpose only  """
+    # This method for Testing purpose only
 
     print(Cars.head())
 
 
 def SetTestValues (TestValues = []):
-    """ This Method will take a Dict insted of list in the Future """
+    #   This Method will take a Dict instead of list in the Future
     listOfTValues = TestValues
 
     return listOfTValues
 
 def Model(valueList = []):
 
-    '''
+    
 
-    in this example  i choose  random values ,
-    in the future this indxes will depend on the user's selction
-    in this demo the user want us to recommedd a car with :
-    1- Nummber of galons per mile   ,  2- the Horse Power
-    3- Number of gears of the car
+    #in this example  i choose  random values ,
+    #in the future this indexes will depend on the user's selection
+    #in this demo the user want us to recommend a car with :
+    #1- Nummber of galons per mile   ,  2- the Horse Power
+    #3- Number of gears of the car
 
-    '''
-    '''
-    :return array of 2 arrays 
-    1st: the length between the begining of the csv file  to the choosen value 
-    2nd: the choosen value that well be recomended to the user
-    '''
+    
+    
+    #:return array of 2 arrays 
+    #1st: the length between the beginning of the csv file  to the chosen value 
+    #2nd: the chosen value that well be recommended to the user
+    
     Data = Cars.ix[:, (1, 4, 10)].values
 
     Neighbors = NearestNeighbors(n_neighbors=1).fit(Data)
@@ -59,9 +113,9 @@ def Model(valueList = []):
 
 
 def outputHandling(output):
-    """
-    :return: Full Details for the Recommended Item
-    """
+    
+    #:return: Full Details for the Recommended Item
+    
 
     # cast the list given to String
     OutPut = str(output[1])
@@ -84,3 +138,4 @@ Test = SetTestValues([21,150,4])
 op = Model(Test)
 o = outputHandling(op)
 print(o)
+'''
