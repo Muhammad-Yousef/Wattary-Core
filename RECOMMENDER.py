52cb4e8be54b4f8e1da640eb436d6c291cc3629d
Muhammad Omar <Muhammad.Omar.EG@gmail.com>
2017-10-23 18:17:40 +0200

    Making small changes on the code and removing unnecessary comments

--- RECOMMENDER.py
+++ RECOMMENDER.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import NearestNeighbors
 
 #----------------------------------------------------- OOP Version -----------------------------------#
 class RECOMMENDER:
-    def __init__(self, dataSet):
+    def __init__(self):
         self.dataSet = pd.read_csv('DataSets/mtcars.csv')
 
     def Initialize(self):
@@ -71,84 +71,13 @@ class RECOMMENDER:
 
         # Return the Recommended Item
         self.recommendedItem = self.dataSet.iloc[self.Index]
-        print(self.recommendedItem)
+        return self.recommendedItem
 
-#--------------------------------------------------Main To Test---------------------------------------#
-x=RECOMMENDER("")
-x.outPutHandling([2,15,4])
+#--------------------------------------------------Just for Testing---------------------------------------#
+x=RECOMMENDER()
+x.Initialize()
+listV = x.SetTestValues([21,150,4])
+out = x.Model(listV)
+recomendedItem = x.outPutHandling(out)
+print(recomendedItem)
 
-  #------------------------------------- Old Version of the code without OOP paradigm-------------------#
-'''
-# using pandas library to read our Data set file.csv
-Cars = pd.read_csv('DataSets/mtcars.csv')
-
-def Initlaize():
-
-    # Define the Columns's names of the Data set
-    Cars.columns = ['carNames', 'Mpg', 'Cyl', 'disp', 'horsePower', 'drat', 'Wight', 'qsec', 'vs', 'am', 'Gear', 'carb']
-
-
-def Display():
-    # This method for Testing purpose only
-
-    print(Cars.head())
-
-
-def SetTestValues (TestValues = []):
-    #   This Method will take a Dict instead of list in the Future
-    listOfTValues = TestValues
-
-    return listOfTValues
-
-def Model(valueList = []):
-
-    
-
-    #in this example  i choose  random values ,
-    #in the future this indexes will depend on the user's selection
-    #in this demo the user want us to recommend a car with :
-    #1- Nummber of galons per mile   ,  2- the Horse Power
-    #3- Number of gears of the car
-
-    
-    
-    #:return array of 2 arrays 
-    #1st: the length between the beginning of the csv file  to the chosen value 
-    #2nd: the chosen value that well be recommended to the user
-    
-    Data = Cars.ix[:, (1, 4, 10)].values
-
-    Neighbors = NearestNeighbors(n_neighbors=1).fit(Data)
-
-    Output = Neighbors.kneighbors([valueList])
-
-    return Output
-
-
-def outputHandling(output):
-    
-    #:return: Full Details for the Recommended Item
-    
-
-    # cast the list given to String
-    OutPut = str(output[1])
-
-    # remove the Brackets from the string
-    newOutput = OutPut.strip("[]")
-
-    # cast it again to Integer
-    Index = int(newOutput)
-
-    # Return the Recommended Item
-    recommendedItem = Cars.iloc[Index]
-    return recommendedItem
-
-#--------------------------------------------Just For Testing----------------------------------------------#
-
-Initlaize()
-#Display()
-Test = SetTestValues([21,150,4])
-op = Model(Test)
-o = outputHandling(op)
-print(o)
-'''
