be2a812822d1eba42f6f1f16afdf5a03c12e0928
Muhannad Raouf <mohanadraouf95@gmail.com>
2017-10-24 21:04:51 +0200

    Modifying on the object creation ,init() and setTestValues functions

--- RECOMMENDER.py
+++ RECOMMENDER.py
@@ -2,35 +2,37 @@
 
 # Note: This file Require Numpy , Pandas and Sci-kit learn  Modules
 
-#Importing the modules
+# Importing the modules
 import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.neighbors import NearestNeighbors
 
-#----------------------------------------------------- OOP Version -----------------------------------#
+
+# ----------------------------------------------------- OOP Version -----------------------------------#
 class RECOMMENDER:
-    def __init__(self):
-        self.dataSet = pd.read_csv('DataSets/mtcars.csv')
+    def __init__(self, dataset, testValues=[]):
+        self.dataSet = dataset  # To initialize the path when creating the object
+        self.listOfValues = testValues  # To initialize the test values when crating the object without using a function
 
     def Initialize(self):
         # Define the Columns's names of the Data set
-        self.dataSet.coulmns = ['carNames', 'Mpg', 'Cyl', 'disp', 'horsePower', 'drat', 'Wight', 'qsec', 'vs', 'am', 'Gear', 'carb']
+        self.dataSet.coulmns = ['carNames', 'Mpg', 'Cyl', 'disp', 'horsePower', 'drat', 'Wight', 'qsec', 'vs', 'am',
+                                'Gear', 'carb']
 
     def Display(self):
-
         # This method for Testing purpose only
         print(self.dataSet.head())
 
-
-    def SetTestValues(self, testValues = []):
-        # This Method will take a Dict instead of list in the Future
+    """
+    def SetTestValues(self, testValues=[]):
+        # This Method will take a Dict instead of list in the Future    # Lines to Remove
         self.listOfValues = testValues
 
         return self.listOfValues
+    """
 
-
-    def Model(self, valueList = []):
+    def Model(self, valueList=[]):
         """
             in this example  i choose  random values ,
             in the future this indexes will depend on the user's selection
@@ -48,16 +50,14 @@ class RECOMMENDER:
         self.Data = self.dataSet.ix[:, (1, 4, 10)].values
 
         # Fitting the Nearest Neighbors function to the data in our data set
-        self.Neighbors = NearestNeighbors(n_neighbors = 1).fit(self.Data)
+        self.Neighbors = NearestNeighbors(n_neighbors=1).fit(self.Data)
 
         # Getting the nearest value to the desired values in the data set
         self.Output = self.Neighbors.kneighbors([valueList])
 
         return self.Output
 
-
-
-    def outPutHandling(self,output):
+    def outPutHandling(self, output):
         #:return: Full Details for the Recommended Item
 
         # cast the list given to String
@@ -73,11 +73,12 @@ class RECOMMENDER:
         self.recommendedItem = self.dataSet.iloc[self.Index]
         return self.recommendedItem
 
-#--------------------------------------------------Just for Testing---------------------------------------#
-x=RECOMMENDER()
+
+# --------------------------------------------------Just for Testing---------------------------------------#
+pathOfDataSet = pd.read_csv('DataSets/mtcars.csv')
+x = RECOMMENDER(pathOfDataSet, [21, 150, 4])
 x.Initialize()
-listV = x.SetTestValues([21,150,4])
-out = x.Model(listV)
+# listV = x.SetTestValues([21, 150, 4]) # line to remove
+out = x.Model(x.listOfValues)
 recomendedItem = x.outPutHandling(out)
 print(recomendedItem)
-
