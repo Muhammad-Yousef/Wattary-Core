cbd57ebb62229044e48070785f4b6af9528512e7
Mosaab Ahmed Mohamed <mosabahmeddev@gmail.com>
2018-02-10 21:56:40 +0200

    channing the Model to Decision tree Classifier

--- Core/AirConditionerClassification.py
+++ Core/AirConditionerClassification.py
@@ -1,8 +1,8 @@
 #importing some library for the model
 import pandas as pd
 import numpy as np
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn.tree import  DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
 
 Air=np.asarray([(1,16,30,32,1),
                 (2,16.30,29,32,1),
@@ -17,7 +17,50 @@ Air=np.asarray([(1,16,30,32,1),
                 (11,17,29,31,1),
                 (12,16.45,25,28,0),
                 (13,16.55,25,28,0),
-                (14,16.30,24,27,0)],
+                (14,16.30,24,27,0),
+                (1, 16, 30, 32, 1),
+                (2, 16.30, 29, 32, 1),
+                (3, 16.25, 28, 30, 1),
+                (4, 17, 29, 31, 1),
+                (5, 16.45, 25, 28, 0),
+                (6, 16.55, 25, 28, 0),
+                (7, 16.30, 24, 27, 0),
+                (8, 16, 30, 32, 1),
+                (9, 16.30, 29, 32, 1),
+                (10, 16.25, 28, 30, 1),
+                (11, 17, 29, 31, 1),
+                (12, 16.45, 25, 28, 0),
+                (13, 16.55, 25, 28, 0),
+                (14, 16.30, 24, 27, 0),
+                (1,16,30,32,1),
+                (2,16.30,29,32,1),
+                (3,16.25,28,30,1),
+                (4,17,29,31,1),
+                (5,16.45,25,28,0),
+                (6,16.55,25,28,0),
+                (7,16.30,24,27,0),
+                (8,16,30,32,1),
+                (9,16.30,29,32,1),
+                (10,16.25,28,30,1),
+                (11,17,29,31,1),
+                (12,16.45,25,28,0),
+                (13,16.55,25,28,0),
+                (14,16.30,24,27,0),
+                (1, 16, 30, 32, 1),
+                (2, 16.30, 29, 32, 1),
+                (3, 16.25, 28, 30, 1),
+                (4, 17, 29, 31, 1),
+                (5, 16.45, 25, 28, 0),
+                (6, 16.55, 25, 28, 0),
+                (7, 16.30, 24, 27, 0),
+                (8, 16, 30, 32, 1),
+                (9, 16.30, 29, 32, 1),
+                (10, 16.25, 28, 30, 1),
+                (11, 17, 29, 31, 1),
+                (12, 16.45, 25, 28, 0),
+                (13, 16.55, 25, 28, 0),
+                (14, 16.30, 24, 27, 0),
+                ],
                  dtype=float)
 
 #convert the array into Datafarme
@@ -31,7 +74,7 @@ df.columns=['day','date','interior val','exterior val','user_value']
 X,Y =df.iloc[:,0:4],df.iloc[:,4]
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=1,stratify=Y)
 
-Clssifier=SVC()
+Clssifier=DecisionTreeClassifier(criterion='entropy',random_state=0)
 #fitting the Classfier in DataSet
 Clssifier.fit(X,Y)
 
