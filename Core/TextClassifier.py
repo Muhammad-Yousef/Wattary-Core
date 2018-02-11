3fdbc13340c657f029b9c2b27b7928cfb3c784cb
Omaroovee <Muhammad.omar.eg@gmail.com>
2018-02-12 01:12:35 +0200

    Fixing the Error

--- Core/TextClassifier.py
+++ Core/TextClassifier.py
@@ -3,13 +3,10 @@ from textblob.classifiers import NaiveBayesClassifier
 
 trainList = []
 testList = []
-TC = 'cl'
 
 
-def Train(trainList=[]):
-    """asigning the Training data to a List of Tupples"""
 
-    trainList = [
+trainList = [
         ('Switch on the light', 'light_on'),
         ('turn on the light', 'light_on'),
         ('open the light', 'light_on'),
@@ -17,46 +14,35 @@ def Train(trainList=[]):
         ('Switch off the light', 'light_off'),
         ('turn off the light', 'light_off'),
         ('close the ligh', 'light_off')
-    ]
+]
 
 
-def Test(testList = []):
-    """asigning the Testing data to a List of Tupples"""
 
-    testList = [
 
-            ('Switch on the light', 'light_on'),
-            ('turn on the light', 'light_on'),
-            ('open the light', 'light_on'),
-            ('open up the light', 'light_on'),
-            ('Switch off the light', 'light_off'),
-            ('turn off the light', 'light_off'),
-            ('close the ligh', 'light_off'),
-            ('switch the light on', 'light_on'),
-            ('switch the light off', 'light_off')
-    ]
+testList = [
 
-   
-   
-def TextClassify():
-    """The Process of Text Classification"""
-
-    '''
-    Making an object of NaiveBayesClassifier
-    and call the constructor Function and pass the train data as parameter        
-    '''
-    TC = NaiveBayesClassifier(testList)
+        ('Switch on the light', 'light_on'),
+        ('turn on the light', 'light_on'),
+        ('open the light', 'light_on'),
+        ('open up the light', 'light_on'),
+        ('Switch off the light', 'light_off'),
+        ('turn off the light', 'light_off'),
+        ('close the ligh', 'light_off'),
+        ('switch the light on', 'light_on'),
+        ('switch the light off', 'light_off')
+]
 
    
-def result(Text):
+   
+obj = NaiveBayesClassifier(testList)
+
+   
+def result(Te):
     """Return the Output of Text Classification"""
+    
+    return obj.classify(Te) 
 
-    return TC.classify(Text)
 
-
-Train()
-Test()
-TextClassify()
 print(result("switch on the light"))
 
-
+print(result("turn the light off"))
