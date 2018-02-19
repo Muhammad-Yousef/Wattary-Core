a801f2b793440aa731be3ed63d034ff7e34ad311
Muhammad Yousef <Muhammad.wd@outlook.com>
2018-02-20 01:45:09 +0200

    Updating the intent detector

--- Core/IntentClassifier.py
+++ Core/IntentClassifier.py
@@ -24,9 +24,11 @@ train = [
 
     ('switch on the light', 'light-on'),
     ('turn on the light', 'light-on'),
+    ('open up the light', 'light-on'),
 
     ('switch off the light', 'light-off'),
     ('turn off the light', 'light-off'),
+    ('close the light', 'light-off'),
 
     ('did you switch on the light', 'light-on-query'),
     ('have you switched on the light', 'light-on-query'),
@@ -38,6 +40,14 @@ train = [
 
     ('set the temperature to', 'temperature-update'),
     ('update the temperature to ', 'temperature-update'),
+
+    ("did you set the temperature to", 'temperature-update-query'),
+    ("did you set the air conditioner", 'temperature-update-query'),
+    ("did you update the temperature to", "temperature-update-query"),
+
+    ("have you set the temperature to", "temperature-update-query"),
+    ("have you set the air conditioner to", "temperature-update-query"),
+    ("have you updated the temperature to", "temperature-update-query"),
 
 # -------------------- air conditioning ------------------------- #
 
@@ -57,15 +67,33 @@ train = [
 
     ('switch on the television', 'television-on'),
     ('turn on the television', 'television-on'),
+    ('switch on the tv', 'television-on'),
+    ('turn on the tv', 'television-on'),
 
     ('did you switch on the television', 'television-on-query'),
     ('have you switched on the television', 'television-on-query'),
+    ('did you switch on the tv', 'television-on-query'),
+    ('have you switched on the tv', 'television-on-query'),
+
+    ('did you turn on the television', 'television-on-query'),
+    ('have you turned on the television', 'television-on-query'),
+    ('did you turn on the tv', 'television-on-query'),
+    ('have you turned on the tv', 'television-on-query'),
 
     ('switch off the television', 'television-off'),
     ('turn off the television', 'television-off'),
+    ('switch off the tv', 'television-off'),
+    ('turn off the tv', 'television-off'),
 
     ('did you switch off the television', 'television-off-query'),
     ('have you switched off the television', 'television-off-query'),
+    ('did you switch off the tv', 'television-off-query'),
+    ('have you switched off the tv', 'television-off-query'),
+
+    ('did you turn off the television', 'television-off-query'),
+    ('have you turned off the television', 'television-off-query'),
+    ('did you turn off the tv', 'television-off-query'),
+    ('have you turned off the tv', 'television-off-query'),
 
 # -------------------- Elevator ------------------------- #
 
@@ -74,14 +102,27 @@ train = [
     ('did you call the elevator', 'elevator-calling-query'),
     ('have you called the elevator', 'elevator-calling-query'),
 
+# -------------------- Door ------------------------- #
+
+    ('open the door', 'door-opening'),
+    ('did you open the door', 'door-opening-query'),
+    ('have you opened the door', 'door-opening-query'),
 
 # -------------------- Car Engine ------------------------- #
 
     ('switch on the car engine', 'car-engine-on'),
+    ('switch on the engine of the car', 'car-engine-on'),
+    ('turn on the engine of the car', 'car-engine-on'),
     ('turn on the car engine', 'car-engine-on'),
+    ('did you switch on the car engine', 'car-engine-on-query'),
+    ('have you switched on the car engine', 'car-engine-on-query'),
 
     ('switch off the car engine', 'car-engine-off'),
+    ('switch off the engine of the car', 'car-engine-off'),
+    ('turn off the engine of the car', 'car-engine-off'),
     ('turn off the car engine', 'car-engine-off'),
+    ('did you switch off the car engine', 'car-engine-off-query'),
+    ('have you switched off the car engine', 'car-engine-off-query'),
 
 # -------------------- Weather inquiry ------------------------- #
 
