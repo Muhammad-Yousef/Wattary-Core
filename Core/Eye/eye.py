7649cba536eb9909c4679f9c79d001143d9942b2
Ahmed Abdeldaim <a7medabdeldaim@gmail.com>
2018-04-16 05:07:19 +0200

    Add Eye API

--- Core/Eye/eye.py
+++ Core/Eye/eye.py
@@ -1,85 +1,49 @@
-#
-# Eye API Version 1.0
-#
-# This module plays as a mid-way communicator between the Eye and users
-# To easily detect and add a new user to database and check if this face exist or not
-#
-# Written By: Ahmed Abdeldaim
-#
-# To use this module well:
-# please read the following lines.
-#
-# To load this module in your script, use:
-# >>> from Eye import eye
-# or
-# >>> import Eye.eye
-# or
-# >>> import Eye.eye as anyName
-##############################################################################################
-#
-# To add a user to the database
-# use function register(params)
-# - this function takes 2 parameters:
-#   1- User name. Ex: Sayed Mahmoud
-#   2- a full url of his picture. Ex: https://www.pictures.com/img_002314562.jpg
-#
-# - and returning 1 value:
-#   1- code. Ex: 101.
-#
-#   code meaning:
-#   101: this means the operation succeeded.
-#   102: this means that I can not read the picture (not Exist).
-#   103: this means that I can not find any faces in the picture (retake a picture)
-#   104: this means that the user is exist.
-#   105: this means a memory (database) error.
-#
-# Ex:
-#   code = register('Ahmed Abdeldaim', 'https://www.pictures.com/img_002314562.jpg')
-#
-#
-# To recognize a face from a picture
-# use function login(params)
-# - this function takes 1 parameters:
-#   1- a full url of his picture. Ex: https://www.pictures.com/img_002314562.jpg
-#
-# - and returning 2 value:
-#   1- code. Ex: 201.
-#   2- userID (if exist else null)
-#
-#   code meaning:
-#   201: this means that the user is exist.
-#   202: this means that I can not read the picture (not Exist).
-#   203: this means that I can not find any faces in the picture (retake a picture)
-#   204: this means that the user is exist.
-#   205: this means a memory (database) error.
-#   206: this means that I can not recognize this person.
-#
-# Ex:
-#   code, userID = login('https://www.pictures.com/img_002314562.jpg')
-#
-# Import the database API from the parent directory.
-from Core.Memory.memory import *
-#from faceRecognitionDemo
-from recognize import *
+# Please Fix the path to use this model well
+import Memory
 from skimage import io
+import csv
+import dlib
 import numpy
+
+predictor_path = './models/shape_predictor_5_face_landmarks.dat'
+face_rec_model_path = './models/dlib_face_recognition_resnet_model_v1.dat'
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor(predictor_path)
+facerec = dlib.face_recognition_model_v1(face_rec_model_path)
 
 
 def register(userName, imgPath):
+    # Check if the user is exist
+    code, user_id = login(imgPath)
+    if code == 201:
+        # 104: this means that the user is exist.
+        return 104
+    # If the user is not exist
     try:
         img = io.imread(imgPath)
     except:
+        # 102: this means that I can not read the picture (not Exist).
         return 102
-    code, disc = recognize.getDiscriptor(img)
-    if code == 103:
-        return code
-    code, u = checkUser(disc)
-    if code:
-        return code
-    face_descriptor = ', '.join(str(i) for i in list(disc))
-    code = Memory.insertValues('users', {'user_name': userName,
-                                         'user_face': imgPath,
-                                         'face_descriptor': '{' + face_descriptor + '}'})
+
+    dets = detector(img, 1)
+    face_descriptor = ''
+    is_face = False
+    for k, d in enumerate(dets):
+        is_face = True
+        shape = sp(img, d)
+        face_descriptor = facerec.compute_face_descriptor(img, shape)
+
+    if not is_face:
+        # 103: this means that I can not find any faces in the picture (retake a picture)
+        return 103
+
+    code, ID = Memory.insertValues('users', {'user_name': userName})
+    with open('./users_descriptors.csv', 'a') as o:
+        fieldnames = ['user_ID', 'descriptor']
+        writer = csv.DictWriter(o, fieldnames=fieldnames)
+        writer.writerow({'user_ID': ID, 'descriptor': face_descriptor})
+
     if code == 201:
         return 101
     else:
@@ -91,30 +55,36 @@ def login(imgPath):
         img = io.imread(imgPath)
     except:
         return 202
-    code, disc = recognize.getDiscriptor(img)
-    if code == 203:
-        return code
-    code, u = checkUser(disc)
-    if code == 104:
-        return 201, u
-    else:
-        return 206, ''
 
+    dets = detector(img, 1)
+    face_descriptor = ''
+    is_face = False
+    for k, d in enumerate(dets):
+        is_face = True
+        shape = sp(img, d)
+        face_descriptor = numpy.asarray(list(facerec.compute_face_descriptor(img, shape)), dtype='float32')
 
-def checkUser(discriptor):
-    code, data = Memory.getValuesAll('users', ['face_descriptor'])
-    if code != 401:
-        return 105
-    userName = 'Unknown'
+    if not is_face:
+        return 203, ''
+
     val = 0.5
-    for row in data:
-        savedDist = numpy.asarray(row[0], dtype='float32')
-        dist = numpy.linalg.norm(discriptor - savedDist)
-        if dist < val:
-            userName = 'known'
+    user_id = 0
 
-    if userName != 'Unknown':
-        # IF user is Exist
-        return 104, userName
+    with open('./users_descriptors.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            j = numpy.asarray(row['descriptor'].split('\n'), dtype='float32')
+            label = row['user_ID']
+            dist = numpy.linalg.norm(face_descriptor - j)
+            if dist < val:
+                val = dist
+                user_id = label
+
+    # If user is exist
+    if user_id != 0:
+        return 201, user_id
+        # If user is not exist
     else:
-        return 0, userName
+        return 204, ''
+
+
